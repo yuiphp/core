7551b6d5abddfd3c83e03187c4c1a80701a7530d
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-05-23 22:23:36 -0300

    tests: operators new tests

--- tests/src/ORM/Operators/OperatorsTest.php
+++ tests/src/ORM/Operators/OperatorsTest.php
@@ -8,7 +8,17 @@ it('should return a string with the equal operator', function () {
     $column = 'name';
     $value = 'John Doe';
 
-    $expected = " WHERE $column = '$value' ";
+    $expected = "$column = '$value' ";
+    $actual = eq($column, $value);
+
+    expect($actual)->toBe($expected);
+});
+
+it('should return without quotes if the value is an integer in the equal operator', function () {
+    $column = 'age';
+    $value = 18;
+
+    $expected = "$column = $value ";
     $actual = eq($column, $value);
 
     expect($actual)->toBe($expected);
@@ -18,7 +28,17 @@ it('should return a string with the not equal operator', function () {
     $column = 'name';
     $value = 'John Doe';
 
-    $expected = " WHERE $column != '$value' ";
+    $expected = "$column != '$value' ";
+    $actual = neq($column, $value);
+
+    expect($actual)->toBe($expected);
+});
+
+it('should return without quotes if the value is an integer in the not equal operator', function () {
+    $column = 'age';
+    $value = 18;
+
+    $expected = "$column != $value ";
     $actual = neq($column, $value);
 
     expect($actual)->toBe($expected);
@@ -28,7 +48,7 @@ it('should return a string with the greater than operator', function () {
     $column = 'age';
     $value = 18;
 
-    $expected = " WHERE $column > '$value' ";
+    $expected = "$column > $value ";
     $actual = gt($column, $value);
 
     expect($actual)->toBe($expected);
@@ -38,7 +58,7 @@ it('should return a string with the greater than or equal operator', function ()
     $column = 'age';
     $value = 18;
 
-    $expected = " WHERE $column >= '$value' ";
+    $expected = "$column >= $value ";
     $actual = gte($column, $value);
 
     expect($actual)->toBe($expected);
@@ -48,7 +68,7 @@ it('should return a string with the less than operator', function () {
     $column = 'age';
     $value = 18;
 
-    $expected = " WHERE $column < '$value' ";
+    $expected = "$column < $value ";
     $actual = lt($column, $value);
 
     expect($actual)->toBe($expected);
@@ -58,7 +78,7 @@ it('should return a string with the less than or equal operator', function () {
     $column = 'age';
     $value = 18;
 
-    $expected = " WHERE $column <= '$value' ";
+    $expected = "$column <= $value ";
     $actual = lte($column, $value);
 
     expect($actual)->toBe($expected);
@@ -69,7 +89,7 @@ it('should return a string with the between operator', function () {
     $value1 = 18;
     $value2 = 25;
 
-    $expected = " WHERE $column BETWEEN '$value1' AND '$value2' ";
+    $expected = "$column BETWEEN $value1 AND $value2 ";
     $actual = between($column, $value1, $value2);
 
     expect($actual)->toBe($expected);
@@ -80,7 +100,7 @@ it('should return a string with the not between operator', function () {
     $value1 = 18;
     $value2 = 25;
 
-    $expected = " WHERE $column NOT BETWEEN '$value1' AND '$value2' ";
+    $expected = "$column NOT BETWEEN $value1 AND $value2 ";
     $actual = notBetween($column, $value1, $value2);
 
     expect($actual)->toBe($expected);
@@ -90,7 +110,7 @@ it('should return a string with the in array operator', function () {
     $column = 'age';
     $values = [18, 25, 30];
 
-    $expected = " WHERE $column IN ('18', '25', '30') ";
+    $expected = "$column IN (18, 25, 30) ";
     $actual = inArray($column, $values);
 
     expect($actual)->toBe($expected);
@@ -100,7 +120,7 @@ it('should return a string with the not in array operator', function () {
     $column = 'age';
     $values = [18, 25, 30];
 
-    $expected = " WHERE $column NOT IN ('18', '25', '30') ";
+    $expected = "$column NOT IN (18, 25, 30) ";
     $actual = notInArray($column, $values);
 
     expect($actual)->toBe($expected);
@@ -108,40 +128,51 @@ it('should return a string with the not in array operator', function () {
 
 it('should return a string with the like operator', function () {
     $column = 'name';
-    $value = 'John';
+    $value = '%John%';
 
-    $expected = " WHERE $column LIKE '%John%' ";
+    $expected = "$column LIKE '%John%' ";
     $actual = like($column, $value);
 
     expect($actual)->toBe($expected);
 });
 
 it('should return a string with the ilike operator', function () {
+    $_ENV['DB_CONNECTION'] = 'pgsql';
     $column = 'name';
-    $value = 'John';
+    $value = '%John%';
 
-    $expected = " WHERE $column ILIKE '%John%' ";
+    $expected = "$column ILIKE '%John%' ";
     $actual = ilike($column, $value);
+
+    expect($actual)->toBe($expected);
+    $_ENV['DB_CONNECTION'] = '';
+});
+
+it('should return a string with the not ilike operator', function () {
+    $column = 'name';
+    $value = '%John%';
+
+    $expected = "$column NOT ILIKE '%John%' ";
+    $actual = notIlike($column, $value);
 
     expect($actual)->toBe($expected);
 });
 
 it('should return a string with the exists operator', function () {
-    $column = 'name';
-    $query = "SELECT * FROM users WHERE name = testing";
+    $query = "SELECT * FROM users  name = testing";
 
-    $expected = " WHERE EXISTS (SELECT * FROM users WHERE name = testing) ";
-    $actual = exists($column, $query);
+    $expected = "EXISTS (SELECT * FROM users  name = testing) ";
+    $actual = exists($query);
 
     expect($actual)->toBe($expected);
 });
 
 it('should return a string with the not exists operator', function () {
     $column = 'name';
-    $query = "SELECT * FROM users WHERE name = testing";
+    $query = "SELECT * FROM users  name = testing";
 
-    $expected = " WHERE NOT EXISTS (SELECT * FROM users WHERE name = testing) ";
-    $actual = notExists($column, $query);
+    $expected = "NOT EXISTS (SELECT * FROM users  name = testing) ";
+    $actual = notExists($query);
 
     expect($actual)->toBe($expected);
 });
@@ -149,7 +180,7 @@ it('should return a string with the not exists operator', function () {
 it('should return a string with the is null operator', function () {
     $column = 'name';
 
-    $expected = " WHERE $column IS NULL ";
+    $expected = "$column IS NULL ";
     $actual = isNull($column);
 
     expect($actual)->toBe($expected);
@@ -158,7 +189,7 @@ it('should return a string with the is null operator', function () {
 it('should return a string with the is not null operator', function () {
     $column = 'name';
 
-    $expected = " WHERE $column IS NOT NULL ";
+    $expected = "$column IS NOT NULL ";
     $actual = isNotNull($column);
 
     expect($actual)->toBe($expected);
